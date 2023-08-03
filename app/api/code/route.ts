f50e65dad5e2f22128f694359a6d983c4dcef549
Abhi Shek <polampallyabhishek890@gmail.com>
2023-08-03 20:16:37 +0530

    Other AI Tools added

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -1,12 +1,18 @@
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
-import { Configuration, OpenAIApi } from "openai";
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
+
+const instructionMessage: ChatCompletionRequestMessage = {
+  role: "system",
+  content:
+    "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations.",
+};
 
 export async function POST(request: Request) {
   try {
@@ -23,12 +29,12 @@ export async function POST(request: Request) {
     }
 
     if (!messages) {
-      return new NextResponse("Messages are required", { status: 400 });
+      return new NextResponse("Messages are required", { status: 401 });
     }
 
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages,
+      messages: [instructionMessage, ...messages],
     });
 
     return NextResponse.json(response.data.choices[0].message);
